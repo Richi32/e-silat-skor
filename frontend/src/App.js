9a1d83b84be5024ebb10184d60b34bb624e69695
risema <richi2001.rsm@gmail.com>
2023-06-30 18:35:44 +0700

    pembuatan frontend
    push ke 1 (ini corenya)
    fitur :
    - navbar (menggunakan adminlte dan boostrap)

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from "react";
+import Navbar from "./Navbar";
+import PageSabung from "./PageSabung";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;      
